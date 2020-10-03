eb0c8ef9d5bea8120a4fb05a035b43e0befbec28
David Guo <david.dw.guo@gmail.com>
2020-10-03 16:41:34 -0700

    Sign In/Out button features via firebase

---
+++ src/components/SignIn.vue
@@ -0,0 +1,37 @@
+<template>
+    <section>
+        <!-- <navigation></navigation> -->
+        <!-- <h5 class="center-align">Login</h5> -->
+        <section id="firebaseui-auth-container"></section>
+    </section>
+</template>
+
+<script>
+// import navigation from "@/components/NavBar.vue";
+import firebase from "firebase";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+export default {
+    name: "SignIn",
+    data() {
+        return {};
+    },
+    components: {
+    },
+    mounted() {
+        let ui = firebaseui.auth.AuthUI.getInstance();
+        if (!ui) {
+            ui = new firebaseui.auth.AuthUI(firebase.auth());
+        }
+        var uiConfig = {
+            signInSuccessUrl: "/", // This redirect can be achived by route using callback.
+            signInFlow: "popup",
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ]
+        };
+        ui.start("#firebaseui-auth-container", uiConfig);
+    }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SignIn from '@/components/SignIn'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/SignIn',
+      name: 'SignIn',
+      component: SignIn,
+      meta: {
+        guest: true
+      }
   }
 ]
 
@@ -26,4 +35,8 @@ const router = new VueRouter({
   routes
 })
 
+
+
 export default router
+
+
