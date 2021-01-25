d8cae836e00bb9f4a21c94d191610723306b09cb
Eric Guo <eric.w.guo@gmail.com>
2021-01-24 22:51:27 -0800

    Added Scartch Games to Activities

--- src/components/Activities.vue
+++ src/components/Activities.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h3>Have Fun With These Activities</h3>
     <iframe
       v-for="(link, i) in links"
       :key="i"
@@ -21,6 +22,8 @@ export default {
         "https://www.youtube.com/embed/oYk0iU2r9VQ",
         "https://www.youtube.com/embed/wWnsVUNOVcY",
         "https://www.youtube.com/embed/2cIXoWpAgTk",
+        "https://sulfurous.aau.at/html/app.html?id=175032129&turbo=false&full-screen=false&aspect-x=&aspect-y=&resolution-x=450&resolution-y=320",
+        "https://sulfurous.aau.at/html/app.html?id=177419227&turbo=false&full-screen=false&aspect-x=&aspect-y=&resolution-x=450&resolution-y=320",
       ],
     };
   },
